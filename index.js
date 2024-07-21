6f13daf233eb2be5cffcd19154454ae9155ed8f6
Arunkanakkillam <arungeo70@gmail.com>
2024-07-21 17:46:09 +0530

    Updated files with new changes

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 (function() {
     emailjs.init("fRI6WqcmvTN5PKyGQ"); 
 })();
@@ -23,4 +24,31 @@ function sendMail(event) {
             alert("Your message was sent successfully");
         })
         .catch(err => console.log(err));
+=======
+(function() {
+    emailjs.init("fRI6WqcmvTN5PKyGQ"); 
+})();
+
+function sendMail(event) {
+    event.preventDefault(); 
+
+    let params = {
+        Email: document.getElementById("exampleFormControlInput1").value,
+        Mobile: document.getElementById("exampleFormControlInput2").value,
+        Message: document.getElementById("exampleFormControlTextarea3").value,
+    }
+
+    const serviceID = "service_j6dkolf";
+    const templateID = "template_uu69y9j";
+
+    emailjs.send(serviceID, templateID, params)
+        .then(res => {
+            document.getElementById("exampleFormControlInput1").value = "";
+            document.getElementById("exampleFormControlInput2").value = "";
+            document.getElementById("exampleFormControlTextarea3").value = "";
+            console.log(res);
+            alert("Your message was sent successfully");
+        })
+        .catch(err => console.log(err));
+>>>>>>> c27c763 (Updated files with new changes)
 }
